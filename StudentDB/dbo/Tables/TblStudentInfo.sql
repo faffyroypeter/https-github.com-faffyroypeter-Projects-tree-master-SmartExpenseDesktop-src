1a08c0c48697d866124d1c74e5b1ef87055a5353
Peter <faffyroypeter@gmail.com>
2020-02-26 02:33:58 +0530

    Update Database project for sql express

---
+++ StudentDB/dbo/Tables/TblStudentInfo.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[TblStudentInfo] (
+    [Id]        INT            IDENTITY (1, 1) NOT NULL,
+    [RoleNo]    NVARCHAR (255) NOT NULL,
+    [FirstName] NVARCHAR (255) NOT NULL,
+    [LastName]  NVARCHAR (255) NOT NULL,
+    [City]      NVARCHAR (255) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
